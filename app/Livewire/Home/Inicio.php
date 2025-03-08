cd2ad7aa0e4c90b8c5f2480a026e1d6d2e2ad8e2
David <davidcarmonaarrabal18@gmail.com>
2025-03-08 18:58:49 +0100

    Added some components as table and card

--- app/Livewire/Home/Inicio.php
+++ app/Livewire/Home/Inicio.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Home;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Inicio test')]
 class Inicio extends Component
 {
     public function render()
--- resources/views/livewire/home/inicio.blade.php
+++ resources/views/livewire/home/inicio.blade.php
@@ -1,3 +1,20 @@
 <div>
     <h1>Inicio</h1>
+
+    <x-card cardTitle="Card Title" cardFooter="Card Footer">
+        <x-slot:cardTools>
+            <a href="#" class="btn btn-primary">Crear</a>
+        </x-slot:cardTools>
+        <x-table>
+            <x-slot:thead>
+                <th>thead</th>
+                <th>thead</th>
+            </x-slot>
+
+            <tr>
+                <td>...</td>
+                <td>...</td>
+            </tr>
+        </x-table>
+    </x-card>
 </div>
