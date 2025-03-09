b3df9919af7af070e88fb6adec74e476e1d898bd
David <davidcarmonaarrabal18@gmail.com>
2025-03-09 19:15:13 +0100

    Empiezo a tocar estilos

--- resources/views/livewire/dashboard/dashboard-component.blade.php
+++ resources/views/livewire/dashboard/dashboard-component.blade.php
@@ -2,33 +2,35 @@
     <div class="row">
         <!-- Total de Cartas -->
         <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">Total de Ofertas</div>
-                <div class="card-body">
-                    <h3>{{ $totalCards }}</h3>
-                    <p>Este es el número total de cartas ofertadas.</p>
+            <a href="{{ route('card') }}">
+                <div class="card">
+                    <div class="card-header">Total de Ofertas</div>
+                    <div class="card-body">
+                        <h3>{{ $totalCards }}</h3>
+                        <p>Este es el número total de cartas ofertadas.</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
 
-        <!-- Cartas del Usuario -->
+        <div class="col-md-4">
+            <a href="{{ route('order') }}">
+                <div class="card">
+                    <div class="card-header">Mis Compras</div>
+                    <div class="card-body">
+                        <h3>{{ $userOrdersCount }}</h3>
+                        <p>Este es el número de cartas que has comprado.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">Mis Ofertas</div>
                 <div class="card-body">
                     <h3>{{ $userCardsCount }}</h3>
                     <p>Este es el número de cartas que has ofertado.</p>
-                </div>
-            </div>
-        </div>
-
-        <!-- Orders del Usuario -->
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">Mis Compras</div>
-                <div class="card-body">
-                    <h3>{{ $userOrdersCount }}</h3>
-                    <p>Este es el número de cartas que has comprado.</p>
                 </div>
             </div>
         </div>
